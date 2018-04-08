6e7a20af2cf43d790fb0e57529539e03d69f92d0
jinma.xu <626875876@qq.com>
2018-04-08 17:06:17 +0800

    冲突解决

--- rmis/src/main/java/com/jfhealthcare/modules/business/service/impl/ViewWorklistServiceImpl.java
+++ rmis/src/main/java/com/jfhealthcare/modules/business/service/impl/ViewWorklistServiceImpl.java
@@ -1,9 +1,12 @@
 package com.jfhealthcare.modules.business.service.impl;
 
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -18,6 +21,7 @@ import com.github.pagehelper.PageHelper;
 import com.jfhealthcare.common.entity.LoginUserEntity;
 import com.jfhealthcare.common.entity.MyPageInfo;
 import com.jfhealthcare.common.enums.CheckStatusEnum;
+import com.jfhealthcare.common.exception.RmisException;
 import com.jfhealthcare.common.utils.DateUtils;
 import com.jfhealthcare.common.utils.NameUtils;
 import com.jfhealthcare.common.validator.Assert;
@@ -124,9 +128,34 @@ public class ViewWorklistServiceImpl implements ViewWorklistService {
 		List<ViewWorklistResponse> vwls=viewWorklistMapper.queryViewWorklist(vwlr);
 		if(!CollectionUtils.isEmpty(vwls)){
 			ViewWorklistResponse viewWorklistResponse = vwls.get(0);
-			String checkNum = viewWorklistResponse.getCheckNum();
-			ApplyWorklist applyWorklist = applyWorklistMapper.selectByPrimaryKey(checkNum);
-			viewWorklistResponse.setSopUrl(webViewUrl+applyWorklist.getStudyUid());
+			String reprcdRepUid = viewWorklistResponse.getReprcdRepUid();
+			RepImage repImage=new RepImage();
+			repImage.setRepUid(reprcdRepUid);
+			List<RepImage> repImages = repImageMapper.select(repImage);
+			List<Map<String, String>> uidmaps=new ArrayList<Map<String, String>>();
+			for (RepImage rimg : repImages) {
+				String[] splits = StringUtils.split(rimg.getImgPage(), "&");
+				Map<String, String> uidmap=new HashMap<String, String>();
+				for (String uids : splits) {
+					String[] keyAndUid = uids.split("=");
+					String uid = keyAndUid[1];
+					if("studyUID".equals(keyAndUid[0])) {
+						uidmap.put("StudyUid", uid);
+					}else if("seriesUID".equals(keyAndUid[0])) {
+						uidmap.put("SeriesUid", uid);
+					}else if("objectUID".equals(keyAndUid[0])) {
+						uidmap.put("ObjectUid", uid);
+					}
+				}
+				uidmaps.add(uidmap);
+			}
+			try {
+				String encode = URLEncoder.encode(JSON.toJSONString(uidmaps),"UTF-8");
+				viewWorklistResponse.setSopUrl(webViewUrl+encode);
+				log.info("web view url:{}", uidmaps);
+			} catch (Exception e) {
+				throw new RmisException("web view encode exception!");
+			}
 			return viewWorklistResponse;
 		}
 		return new ViewWorklistResponse();
